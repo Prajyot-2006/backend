b85fd86a1d247cde3c688034815f9d10cb87a144
Prajyot Jore <prajyotjore06@gmail.com>
2025-09-15 22:52:31 +0530

    Initial commit

---
+++ mongodb/kk.js
@@ -0,0 +1,16 @@
+[
+    {
+        "sName" : "Killua",
+        "sEmail" : "killua.jore@gmail.com"
+    },
+    {
+        "sName" : "Gon",
+        "sEmail" : "gon@gmail.com"
+    },
+    {
+        "sName" : "Levi",
+        "sEmail" : "levi@gmail.com"
+    }
+]
+
+
